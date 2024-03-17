39b4860e1a54853942a2b9bba629cc37d411b625
Gustavo-Soto-Reyes <gsotoreyes73@gmail.com>
2024-03-17 00:46:57 -0700

    set up liveData to handle database reads

--- app/src/main/java/com/example/contact_manager/Repository.java
+++ app/src/main/java/com/example/contact_manager/Repository.java
@@ -1,23 +1,48 @@
 package com.example.contact_manager;
 
+import android.app.Application;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.lifecycle.LiveData;
+
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Repository {
     private final ContactDAO dao;
+    private final Handler handler;
+    private final ExecutorService executor;
 
-    public Repository(ContactDAO dao) {
-        this.dao = dao;
+    public Repository(Application app) {
+
+        ContactDatabase db = ContactDatabase.getInstance(app);
+        this.dao = db.getContactDAO();
+        handler = new Handler(Looper.getMainLooper());
+        executor = Executors.newSingleThreadExecutor();
     }
 
     public void addContact(Contact contact){
-        dao.insert(contact);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.insert(contact);
+            }
+        });
     }
 
     public void deleteContact(Contact contact){
-        dao.delete(contact);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.delete(contact);
+            }
+        });
     }
 
-    public List<Contact> getAllContacts(){
+    public LiveData<List<Contact>> getAllContacts(){
         return dao.getAllContacts();
     }
 
